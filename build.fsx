b9fc0d0a2bf22d5af3c58535f51f69909567445a
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2023-09-05 00:55:43 +0800

    Update build.fsx

--- build.fsx
+++ build.fsx
@@ -1,4 +1,4 @@
-#r "nuget: Fun.Build, 0.4.0"
+#r "nuget: Fun.Build, 0.5.0"
 #r "nuget: Fake.IO.FileSystem, 5.23.0"
 
 open Fake.IO
@@ -6,53 +6,49 @@ open Fake.IO.Globbing.Operators
 open Fun.Build
 
 
-let envCheckStage =
+let options = {|
+    GithubAction = EnvArg.Create("GITHUB_ACTION", description = "Run only in in github action container")
+    NugetAPIKey = EnvArg.Create("NUGET_API_KEY", description = "Nuget api key")
+|}
+
+let stage_checkEnv =
     stage "Check environment" {
-        paralle
-        run "dotnet --version"
-        run "dotnet --list-sdks"
         run "dotnet tool restore"
-        run (fun ctx -> printfn $"""GITHUB_ACTION: {ctx.GetEnvVar "GITHUB_ACTION"}""")
+        run (fun ctx -> printfn $"""github action name: {ctx.GetEnvVar options.GithubAction.Name}""")
     }
 
-let lintStage =
+let stage_lint =
     stage "Lint" {
         stage "Format" {
-            whenNot { envVar "GITHUB_ACTION" }
+            whenNot { envVar options.GithubAction }
             run "dotnet fantomas . -r"
         }
         stage "Check" {
-            whenEnvVar "GITHUB_ACTION"
+            whenEnvVar options.GithubAction
             run "dotnet fantomas . -r --check"
         }
     }
 
-let testStage = stage "Run unit tests" { run "dotnet test" }
+let stage_test = stage "Run unit tests" { run "dotnet test" }
 
 
 pipeline "Fun.Build" {
     description "Build and deploy to nuget"
-    noPrefixForStep
-    envCheckStage
-    lintStage
-    testStage
+    stage_checkEnv
+    stage_lint
+    stage_test
     stage "Build packages" { run "dotnet pack -c Release Fun.Build/Fun.Build.fsproj -o ." }
     stage "Publish packages to nuget" {
-        whenAll {
-            branch "master"
-            whenAny {
-                envVar "NUGET_API_KEY"
-                cmdArg "NUGET_API_KEY"
-            }
-        }
+        whenBranch "master"
+        whenEnvVar options.NugetAPIKey
         run (fun ctx ->
-            let key = ctx.GetCmdArgOrEnvVar "NUGET_API_KEY"
+            let key = ctx.GetEnvVar options.NugetAPIKey.Name
             ctx.RunSensitiveCommand $"""dotnet nuget push *.nupkg -s https://api.nuget.org/v3/index.json --skip-duplicate -k {key}"""
         )
     }
     post [
-        stage "Post stage" {
-            whenNot { envVar "GITHUB_ACTION" }
+        stage "Clean packages" {
+            whenNot { envVar options.GithubAction }
             run (fun _ -> File.deleteAll !! "*.nupkg")
         }
     ]
@@ -61,10 +57,9 @@ pipeline "Fun.Build" {
 
 pipeline "test" {
     description "Format code and run tests"
-    noPrefixForStep
-    envCheckStage
-    lintStage
-    testStage
+    stage_checkEnv
+    stage_lint
+    stage_test
     runIfOnlySpecified
 }
 
