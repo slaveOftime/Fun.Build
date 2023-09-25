02a9615cd0a0196dbe10bf7fffdac1ccb412d9e2
binwen <albertwoo_slaveoftime@hotmail.com>
2023-09-25 16:18:44 +0800

    Add FunBuildAot sample

---
+++ Samples/FunBuildAot/Program.fs
@@ -0,0 +1,30 @@
+﻿open Fun.Build
+open System.Net.Http
+
+pipeline "demo" {
+    stage "run something" {
+        echo "hi"
+        stage "fetch bing" {
+            whenCmdArg "--bing" "" "fetch bing home page"
+            run (fun _ -> task {
+                use client = new HttpClient()
+                let! result = client.GetAsync("https://www.bing.com")
+                if result.IsSuccessStatusCode then
+                    let! content = result.Content.ReadAsStringAsync()
+                    printfn $"Fetch content {content.Length} chars"
+                    return Ok()
+                else
+                    return Error $"Failed with code {result.StatusCode}"
+            })
+        }
+    }
+    runIfOnlySpecified false
+}
+
+pipeline "demo2" {
+    stage "" { echo "cool" }
+    runIfOnlySpecified
+}
+
+
+tryPrintPipelineCommandHelp ()
