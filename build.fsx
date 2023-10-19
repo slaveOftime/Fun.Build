884fa4fc0d035d117efd79a9e2051c843cec1e46
binwen <albertwoo_slaveoftime@hotmail.com>
2023-10-19 14:16:24 +0800

    Update workflow

--- build.fsx
+++ build.fsx
@@ -1,10 +1,5 @@
-#r "nuget: Fun.Build, 0.5.2"
-#r "nuget: Fake.IO.FileSystem, 5.23.0"
+#r "nuget: Fun.Build, 1.0.1"
 
-open System
-open System.IO
-open Fake.IO
-open Fake.IO.Globbing.Operators
 open Fun.Build
 
 
@@ -13,22 +8,6 @@ let options = {|
     NugetAPIKey = EnvArg.Create("NUGET_API_KEY", description = "Nuget api key")
 |}
 
-let getVersionFromChangelogFor dir =
-    File.ReadLines(Path.Combine(dir, "CHANGELOG.md"))
-    |> Seq.tryPick (fun line ->
-        let line = line.Trim()
-        // We'd better to not release unreleased version
-        if "## [Unreleased]".Equals(line, StringComparison.OrdinalIgnoreCase) then
-            None
-        // Simple way to find the version string
-        else if line.StartsWith "## [" && line.Contains "]" then
-            let version = line.Substring(4, line.IndexOf("]") - 4)
-            // In the future we can verify version format according to more rules
-            if Char.IsDigit version[0] |> not then failwith "First number should be digit"
-            Some(version)
-        else
-            None
-    )
 
 let stage_checkEnv =
     stage "Check environment" {
@@ -51,7 +30,7 @@ let stage_lint =
 let stage_test = stage "Run unit tests" { run "dotnet test" }
 
 
-pipeline "Fun.Build" {
+pipeline "packages" {
     description "Build and deploy to nuget"
     stage_checkEnv
     stage_lint
@@ -59,25 +38,20 @@ pipeline "Fun.Build" {
     stage "Build packages" {
         run (fun _ ->
             let version =
-                getVersionFromChangelogFor __SOURCE_DIRECTORY__ |> Option.defaultWith (fun () -> failwith "Version is not found")
-            $"dotnet pack -c Release Fun.Build/Fun.Build.fsproj -p:PackageVersion={version} -o ."
+                Changelog.GetLastVersion(__SOURCE_DIRECTORY__) |> Option.defaultWith (fun () -> failwith "Version is not found")
+            $"dotnet pack -c Release Fun.Build/Fun.Build.fsproj -p:PackageVersion={version.Version} -o ."
         )
     }
     stage "Publish packages to nuget" {
         whenBranch "master"
         whenEnvVar options.NugetAPIKey
+        whenEnvVar "GITHUB_ENV" "" "Only push packages in github action"
         run (fun ctx ->
             let key = ctx.GetEnvVar options.NugetAPIKey.Name
             ctx.RunSensitiveCommand $"""dotnet nuget push *.nupkg -s https://api.nuget.org/v3/index.json --skip-duplicate -k {key}"""
         )
     }
-    post [
-        stage "Clean packages" {
-            whenNot { envVar options.GithubAction }
-            run (fun _ -> File.deleteAll !! "*.nupkg")
-        }
-    ]
-    runIfOnlySpecified false
+    runIfOnlySpecified
 }
 
 pipeline "test" {
