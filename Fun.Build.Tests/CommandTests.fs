5f357867d888c66f89cbccafb001210f66d549a2
binwen <albertwoo_slaveoftime@hotmail.com>
2024-06-24 11:29:42 +0800

    Add more tests

--- Fun.Build.Tests/CommandTests.fs
+++ Fun.Build.Tests/CommandTests.fs
@@ -9,8 +9,8 @@ open Fun.Build
 let ``use cts to cancle a parallel command should work`` () =
     use cts = new CancellationTokenSource()
     let mutable count = 0
-    
-    pipeline "timeout" {
+
+    pipeline "-" {
         stage "tasks" {
             paralle
             stage "timer" {
@@ -21,6 +21,38 @@ let ``use cts to cancle a parallel command should work`` () =
             stage "timer" {
                 whenNot { platformWindows }
                 run "sleep 10" cts.Token
+                run (fun _ -> count <- count + 1)
+            }
+            stage "terminator" {
+                run (fun _ -> async {
+                    do! Async.Sleep(1000)
+                    cts.Cancel()
+                })
+            }
+        }
+        stage "" { run (fun _ -> count <- count + 1) }
+        runImmediate
+    }
+
+    if count <> 2 then Assert.Fail("Should run through all stages/steps success")
+
+
+[<Fact>]
+let ``use cts to cancle a parallel command through ctx.RunCommand should work`` () =
+    use cts = new CancellationTokenSource()
+    let mutable count = 0
+
+    pipeline "-" {
+        stage "tasks" {
+            paralle
+            stage "timer" {
+                whenWindows
+                run (fun ctx -> ctx.RunCommand("timeout /t 10", cancellationToken = cts.Token))
+                run (fun _ -> count <- count + 1)
+            }
+            stage "timer" {
+                whenNot { platformWindows }
+                run (fun ctx -> ctx.RunCommand("sleep 10", cancellationToken = cts.Token))
                 run (fun _ -> count <- count + 1)
             }
             stage "terminator" {
